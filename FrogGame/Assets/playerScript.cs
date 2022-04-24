cfa15491c613d2407870fe1e1a8308dc885985d3
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2022-04-23 18:22:03 -0700

    Added Animation

--- FrogGame/Assets/playerScript.cs
+++ FrogGame/Assets/playerScript.cs
@@ -8,6 +8,7 @@ public class playerScript : MonoBehaviour
 
     private Rigidbody2D rb;
     private BoxCollider2D box2D;
+    public Animator animator;
     public int ForceMultiplier;
 
     
@@ -37,16 +38,28 @@ public class playerScript : MonoBehaviour
             Debug.Log("Mouse Position = " + mousePos);
             Launch(ForceMultiplier * -distanceFromPlayer);
         }
+        
+    }
+    
+    void LateUpdate(){
+        if(rb.velocity.x < -0.1){
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else{
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision){
         if(collision.gameObject.tag == "Platform"){
             playerJumping = false;
+            animator.SetBool("isJumping", false);
         }
     }
     void OnTriggerExit2D(Collider2D collision){
         if(collision.gameObject.tag == "Platform"){
             playerJumping = true;
+            animator.SetBool("isJumping", true);
         }
     }
 
